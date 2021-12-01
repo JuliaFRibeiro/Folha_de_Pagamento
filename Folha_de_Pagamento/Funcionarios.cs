b92c6e0e9fe9191339bf46fd1826b898d62f6f7d
Julia <julia_felix03@hotmail.com>
2021-12-01 15:44:01 -0300

    Adicionar arquivos de projeto.

---
+++ Folha_de_Pagamento/Funcionarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folha_de_Pagamento.Classe
+{
+    abstract class Funcionarios
+    {
+        // declarando variantes base para os funcionarios
+        public String Nome { get; set; }
+        public int Horas { get; set; }
+        public double THoras { get; set; }
+
+        // construtores
+        public Funcionarios() { }
+        public Funcionarios(string nome, int horas, double thoras)
+        {
+            Nome = nome;
+            Horas = horas;
+            THoras = thoras;
+        }
+
+        // função para guardar o valor do pagamento
+        public abstract double Salario();
+    }
+}
---
+++ Folha_de_Pagamento/Tercerizado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Folha_de_Pagamento.Classe
+{
+    class Tercerizado : Funcionarios
+    {
+        // declarando variavel que guarda o valor das despesas
+        public double Adicional { get; set; }
+
+        // construtores
+        public Tercerizado() { }
+        public Tercerizado(string nome, int horas, double thoras, double adicional) : base(nome, horas, thoras)
+        {
+            Nome = nome;
+            Horas = horas;
+            THoras = thoras;
+            Adicional = adicional;
+        }
+        // função que calcula o pagamento dos funcionarios tercerizados
+        public override double Salario()
+        {
+            Adicional = Adicional * 110 / 100;
+            return Horas * THoras + Adicional;
+        }
+    }
+}
